83bd6acb22032767af30d28bb001d8ac58003d92
Mostafa Shahin <mostafa71000shahin@gmail.com>
2020-01-12 14:42:42 +0200

    Update python.py

--- python.py
+++ python.py
@@ -2,8 +2,10 @@
 for t in range(100):
   print('time step: ',t,'\r', end="")
 #or use std.out.flush  
-  
+=============================================================================== 
 #print numby array with rounded values
 np.set_printoptions(formatter={'float': lambda x: "{0:0.2f}".format(x)})
 print(np.array([1.6559995,.015995]))
-
+===============================================================================
+pytorch use .detach() when using calculated targets in loss functions
+===============================================================================
