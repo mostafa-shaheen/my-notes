74e3f800a03bc8b7b46b7dbe754aaf2986f8390d
Mostafa Shahin <mostafa71000shahin@gmail.com>
2020-01-16 14:23:58 +0200

    Update python.py

--- python.py
+++ python.py
@@ -9,3 +9,8 @@ print(np.array([1.6559995,.015995]))
 ===============================================================================
 pytorch use .detach() when using calculated targets in loss functions
 ===============================================================================
+#download dataset from kaggle 
+import os
+os.environ['KAGGLE_USERNAME'] = "mostafa77" # username from the json file
+os.environ['KAGGLE_KEY'] = "347368f5c64dba7e88eaa79d996db0ec" # key from the json file
+!kaggle competitions download favorita-grocery-sales-forecasting -f test.csv.7z
